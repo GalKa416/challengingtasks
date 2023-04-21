21df9a9e694bf2bfb1c5fcc569e9d71d37b7ac5d
Галина <g416@mail.ru>
2023-04-21 22:31:26 +0500

    6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //task 6
+        System.out.println("1");
+        int a = 12;
+        int b = 27;
+        int c= 44;
+        int d= 15;
+        int e = 9;
+        int result = a * (b + (c - d * e));
+        result = result * (-1);
+        System.out.println(result);
+
     }
 }
